b910c4cd417fe3164654da4a0ed8a98c2863e74d
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-04-15 19:10:42 +0530

    Add files via upload

---
+++ private access specifier.py
@@ -0,0 +1,13 @@
+class person:
+    __name=None
+    __bankaccno=None
+    def __init(self):
+        self.__name="hello"
+        self.__bankaccno==1234
+    def display(self):
+        print("name=",self.__name)
+        print("bank number=",self.__bankaccno)
+p=person()
+p.display()
+print("name=",p.__name)
+print("salary=",p.__bankaccno)
